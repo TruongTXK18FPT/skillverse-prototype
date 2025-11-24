85c1c64ae8b92eabe1731e80908e6e03ca3bb8d4
TruongTXK18FPT <truongtranxuan41@gmail.com>
2025-11-24 18:37:02 +0700

    cook

--- src/pages/auth/BusinessRegisterPage.tsx
+++
@@ -1,8 +0,0 @@
-import React from 'react';
-import BusinessRegistrationForm from '../../components/auth/BusinessRegistrationForm';
-
-const BusinessRegisterPage: React.FC = () => {
-  return <BusinessRegistrationForm />;
-};
-
-export default BusinessRegisterPage;
--- src/pages/auth/MentorRegisterPage.tsx
+++
@@ -1,8 +0,0 @@
-import React from 'react';
-import MentorRegistrationForm from '../../components/auth/MentorRegistrationForm';
-
-const MentorRegisterPage: React.FC = () => {
-  return <MentorRegistrationForm />;
-};
-
-export default MentorRegisterPage;
